30ea9543e05d6086110691a8cd4f64808ba30f7e
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-19 01:43:19 -0400

    importing and implementing orbitcontrol

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import './style.css'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 //Scene
 const scene = new THREE.Scene()
@@ -28,9 +29,29 @@ const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 
 camera.position.z = 20
 scene.add(camera)
 
-
 //Renderer
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+//Controls
+const controls = new OrbitControls(camera, canvas)
+
+
+//Resize
+window.addEventListener('resize', () => {
+  //Update Sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //Update Camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+  renderer.render(scene, camera)  
+  window.requestAnimationFrame(loop)
+}
+loop()
